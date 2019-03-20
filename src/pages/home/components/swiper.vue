94690408d1a92a6363faaf2a1c74c332e783ca5d
xuezenghui <xuezenghui6@gmail.com>
2019-03-20 23:56:06 +0800

    change

---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination', // 轮播图小圆点
+          type: 'bullets',
+          clickable: true
+        },
+        autoplay: 3000, // 自动轮播
+        speed: 1000, // 轮播速度
+        loop: true // 无缝轮播
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1507/29/46d244176c9623.jpg_890x330_db953c72.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/813d7d1fe073c569066befe1e4517eeb.jpg_890x330_d0ec0a9a.jpg'
+        }]
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+
+  .wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
