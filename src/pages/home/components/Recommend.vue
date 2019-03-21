6ffafeacef92fdbecc7d980cc31b77394eba9cf3
xuezenghui <xuezenghui6@gmail.com>
2019-03-21 16:14:03 +0800

    index finish

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">猜你喜欢</div>
     <ul>
-      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+      <li class="item border-bottom" v-for="item of list" :key="item.id">
         <img class="item-img"
              :src="item.imgUrl">
         <div class="item-info">
@@ -18,29 +18,8 @@
 <script>
 export default {
   name: 'HomeRecommend',
-  data () {
-    return {
-      recommendList: [
-        {
-          id: '0001',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1504/59/594e60a1366e1741.water.jpg_200x200_b1e2d39d.jpg',
-          title: '华山',
-          desc: '华山论剑，少侠你敢来吗？'
-        },
-        {
-          id: '0002',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1708/76/765e4c83bfdd9ae9a3.img.png_200x200_b331b257.png',
-          title: '中华郡水上乐园',
-          desc: '带给你独一无二的清凉盛宴'
-        },
-        {
-          id: '0003',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/fc/fce82172e1b43b4e.img.jpg_200x200_22962186.jpg',
-          title: '少华山国家森林公园',
-          desc: '关中名山，与华山并称“二华”'
-        }
-      ]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
