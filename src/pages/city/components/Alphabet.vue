8ea5f9f57e2776450c338a65f8db8255181f9cd3
xuezenghui <xuezenghui6@gmail.com>
2019-03-22 00:15:51 +0800

    fix perfomance

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,85 @@
+<template>
+<ul class="list">
+  <li class="item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      @click="handleLetterClick"
+  >
+    {{ item }}
+  </li>
+</ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null // 函数节流
+    }
+  },
+  updated () { // 页面更新时
+    this.startY = this.$refs['A'][0].offsetTop // 获取A离Header底部的高度,在此处计算可优化页面性能
+  },
+  computed: {
+    letters () {
+      const letters = [] // 字母的数组['A','B','C'...]
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => { // 函数节流,如果正在执行handleTouchMove，延迟16毫秒再执行；如果在16毫秒之内再执行，则清除上一次
+          const touchY = e.touches[0].clientY - 79 // 获取拉取时鼠标距离Header底部的高度
+          const index = Math.floor((touchY - this.startY) / 20) // 计算差值，表示到第几个字母
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    right: 0
+    top: 1.58rem
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: #999
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">西安</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper"  v-for="item of hot" :key="item.id">
+            <div class="button">{{ item.name }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="area"
+           v-for="(item,key) of cities"
+           :key="key"
+           :ref="key"
+      >
+        <div class="title border-topbottom">{{ key }}</div>
+        <div class="item-list">
+          <div class="item border-bottom"
+               v-for="innerItem of item"
+               :key="innerItem.id"
+          >
+            {{ innerItem.name }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object,
+    letter: String
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0] // 取数组的第一个元素，即DOM元素
+        this.scroll.scrollToElement(element) // 参数必须为一个DOM元素
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  /*控制页面一像素边框的颜色*/
+  .border-topbottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+
+  .border-bottom
+  &:before
+    border-color: #ccc
+
+  .list
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    bottom: 0
+    right: 0
+    .title
+      line-height: .54rem
+      background: #eee
+      padding-left: .2rem
+      color: #666
+      font-size: .26rem
+
+    .button-list
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .button-wrapper
+        float: left
+        width: 33.33%
+        .button
+          text-align: center
+          margin: .1rem
+          padding: .1rem 0
+          border: .02rem solid #ccc
+          border-radius: .06rem
+    .item-list
+      .item
+        line-height: .76rem
+        color: #666
+        padding-left: .2rem
+</style>
